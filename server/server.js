e092f2e16dab748672deba6bbff61969f27de9ce
rahul kumbhar <rakumbhar5278@gmail.com>
2025-01-31 22:57:28 +0530

    added connection

--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
-import './config/instrument.js'
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/db.js'
-import * as Sentry from "@sentry/node"
-import { clerkWebhooks } from './controllers/webhooks.js'
-import companyRoutes from './routes/companyRoutes.js'
-import connectCloudinary from './config/cloudinary.js'
-import jobRoutes from './routes/jobRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import { clerkMiddleware } from '@clerk/express'
+import './config/instrument.js';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js';
+import companyRoutes from './routes/companyRoutes.js';
+import connectCloudinary from './config/cloudinary.js';
+import jobRoutes from './routes/jobRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
 
-const app = express()
+const app = express();
 
-// CORS configuration
+// Simplified CORS configuration
 app.use(cors({
-    origin: ['https://the-job-company.vercel.app', 'http://localhost:5173'],
+    origin: 'https://the-job-company.vercel.app',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'token']
 }));
 
-app.use(express.json())
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(clerkMiddleware());
 
 // Basic request logging
 app.use((req, res, next) => {
@@ -32,8 +30,8 @@ app.use((req, res, next) => {
 
 // Health check route
 app.get('/', (req, res) => {
-    res.json({ status: 'API is running' })
-})
+    res.json({ status: 'API is running' });
+});
 
 // Routes
 app.use('/api/jobs', (req, res, next) => {
@@ -41,9 +39,9 @@ app.use('/api/jobs', (req, res, next) => {
     next();
 }, jobRoutes);
 
-app.use('/api/company', companyRoutes)
-app.use('/api/users', userRoutes)
-app.post('/webhooks', clerkWebhooks)
+app.use('/api/company', companyRoutes);
+app.use('/api/users', userRoutes);
+app.post('/webhooks', clerkWebhooks);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
